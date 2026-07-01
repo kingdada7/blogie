4313a7af4fd346bd314ffcc3f2d207518c9d8f91
kingdada7 <toluwadada7@gmail.com>
2026-07-01 13:46:16 +0100

    footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return( <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/">
+    <div>
+p
+    </div>
+    <p className="text-white text-center py-4 text-sm md:text-base">
+      &copy; {new Date().getFullYear()} Blogie. All rights reserved.
+    </p>
+  </div>)
+};
+
+export default Footer;
