47a8948002dd6ba8c515eef9a975740751ad7dac
kingdada7 <toluwadada7@gmail.com>
2026-07-02 21:03:06 +0100

    added navbar to blog page

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router";
+import { blog_data } from "../Assets/assets";
+import Navbar from "../components/Navbar";
 
 function Blog() {
-  return (
+  const { id } = useParams();
+
+  const [data, setData] = useState("");
+
+  const fetchBlogData = async () => {
+    const data = blog_data.find((item) => item._id === id);
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+
+  return data ? (
     <div>
-      <h1>blog</h1>
+      <Navbar />
+      <div></div>
+      <div></div>
     </div>
-  )
+  ) : (
+    <div>
+      <h1>loading...</h1>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
