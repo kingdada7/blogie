aeb286925a76506b8ee2ce50badd0ea5dc23d902
kingdada7 <toluwadada7@gmail.com>
2026-07-04 22:09:59 +0100

    currently working on the blog page , so far testing mock data while building the ui

--- frontend/src/components/space/Starfield.jsx
+++ frontend/src/components/space/Starfield.jsx
@@ -1,37 +1,15 @@
-import { useMemo } from "react";
-import * as THREE from "three";
+import { Stars } from "@react-three/drei";
 
 export default function StarField() {
-  const positions = useMemo(() => {
-    const array = new Float32Array(10000 * 3);
-
-    for (let i = 0; i < 10000; i++) {
-      array[i * 3] = (Math.random() - 0.5) * 250;
-      array[i * 3 + 1] = (Math.random() - 0.5) * 250;
-      array[i * 3 + 2] = -Math.random() * 300;
-    }
-
-    return array;
-  }, []);
-
   return (
-    <points>
-      <bufferGeometry>
-        <bufferAttribute
-          attach="attributes-position"
-          count={positions.length / 3}
-          array={positions}
-          itemSize={3}
-        />
-      </bufferGeometry>
-
-      <pointsMaterial
-        color="#8fefff"
-        size={0.12}
-        sizeAttenuation
-        transparent
-        opacity={0.9}
-      />
-    </points>
+    <Stars
+      radius={300}
+      depth={100}
+      count={8000}
+      factor={7}
+      saturation={0}
+      fade
+      speed={0.4}
+    />
   );
 }
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router";
 import { assets, blog_data } from "../Assets/assets";
 import Navbar from "../components/Navbar";
 import SpaceScene from "../components/space/SpaceScene";
-// import SpaceBackground from "../components/SpaceBackground";
+import moment from "moment";
 
 function Blog() {
   const { id } = useParams();
@@ -22,11 +22,26 @@ function Blog() {
 
   return data ? (
     <div className="relative">
-      {/* <SpaceBackground /> */}
       <SpaceScene />
       <Navbar />
-      <div></div>
-      <div></div>
+      <div className="text-center mt-20 text-gray-600">
+        <p className="text-gray-400">
+          Published on {moment(data.createdAt).format("MMMM Do YYYY")}
+        </p>
+        <h1 className="text-gray-300 text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto">
+          {data.title}
+        </h1>
+        <h2 className="text-gray-400 my-5 max-w-lg truncate mx-auto">
+          {data.subTitle}
+        </h2>
+        <p className="text-gray-400 inline-block py-1 px-4 rounded-full mb-6 border text-sm border-cyan/35 bg-primary font-medium ">
+          john doe
+        </p>
+      </div>
+      <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
+        <img src={data.image} alt="rounded-xl mb-5" />
+        <div dangerouslySetInnerHTML={{ __html: data.description }}></div>
+      </div>
     </div>
   ) : (
     <div>
