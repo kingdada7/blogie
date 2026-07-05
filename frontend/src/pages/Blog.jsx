64dc29199f1db59a27ef283f0b8834a04c7fc902
kingdada7 <toluwadada7@gmail.com>
2026-07-05 02:23:12 +0100

    comment section ui done

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -47,13 +47,37 @@ function Blog() {
       <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
         <img src={data.image} alt="rounded-xl mb-5" />
         <div
-          className="rich-text max-w-xl mx-auto"
+          className="rich-text max-w-3xl mx-auto"
           dangerouslySetInnerHTML={{ __html: data.description }}
         ></div>
         {/* comments section */}
         <div className="mt-14 mb-10 max-w-3xl mx-auto">
-          <p className="text-gray-400">Comments({comments.length})</p>
+          <p className="text-gray-400 font-semibold mb-4">Comments({comments.length})</p>
+          <div className="flex flex-col gap-4">
+            {comments.map((item, index) => (
+              <div
+                key={index}
+                className="relative bg-primary/2 border border-primary/35 max-w-xl p-4 rounded text-gray-600"
+              >
+                <div className="flex items-center gap-2 mb-2">
+                  <img src={assets.user_icon} alt="" className="w-4" />
+                  <p className="font-medium">{item.name}</p>
+                </div>
+                <p className="text-gray-400 text-sm max-w-md ml-8">
+                  {item.comment}
+                </p>
+                <div className="absolute right-4 bottom-3 flex items-center gap-2 text-xs">
+                  {moment(item.createdAt).fromNow()}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
+        {/* add a comment section here */}
+<div>
+  
+</div>
+
       </div>
     </div>
   ) : (
