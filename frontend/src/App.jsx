a791c746ce866b3f3c42ebc0e8bf3bfb7592e747
kingdada7 <toluwadada7@gmail.com>
2026-07-05 17:48:02 +0100

    created pages and routes for admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,25 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import SpaceScene from "./components/space/SpaceScene";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddBlog from "./pages/admin/AddBlog";
+import ListBlog from "./pages/admin/ListBlog";
+import Comments from "./pages/admin/Comments";
 
 const App = () => {
   return (
     <div>
-      <SpaceScene/>
+      <SpaceScene />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="listBlog" element={<ListBlog />} />
+          <Route path="comments" element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   );
