fe76b385796478e38dc297b017cccdafc0478604
kingdada7 <toluwadada7@gmail.com>
2026-07-06 15:21:44 +0100

    comments- sidemenu

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router";
 import { assets } from "../../Assets/assets";
+import { House, MessageCircleMore, MessageCirclePlus,ListPlus } from 'lucide-react';
+import { CirclePlus } from "lucide-react";
+
+
 
 const Dashboard = () => {
   return (
@@ -12,7 +16,7 @@ const Dashboard = () => {
           `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-cyan-600/10 border-r-4 border-cyan-600"}`
         }
       >
-        <img src={assets.home_icon} alt="" className="min-w-4 w-5" />
+        <House className="text-cyan-600/40"/>
         <p className="hidden md:inline-block text-white">Dashboard</p>
       </NavLink>
 
@@ -23,7 +27,7 @@ const Dashboard = () => {
           `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-cyan-600/10 border-r-4 border-cyan-600"}`
         }
       >
-        <img src={assets.add_icon} alt="" className="min-w-4 w-5" />
+        <MessageCirclePlus className="text-cyan-600/40"/>
         <p className="hidden md:inline-block text-white">Add blog</p>
       </NavLink>
 
@@ -35,8 +39,20 @@ const Dashboard = () => {
           `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-cyan-600/10 border-r-4 border-cyan-600"}`
         }
       >
-        <img src={assets.list_icon} alt="" className="min-w-4 w-5" />
+       <ListPlus className="text-cyan-600/40"/>
         <p className="hidden md:inline-block text-white">Blog Lists</p>
+      </NavLink>
+
+
+        <NavLink
+        end={true}
+        to="/admin/comments"
+        className={({ isActive }) =>
+          `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-cyan-600/10 border-r-4 border-cyan-600"}`
+        }
+      >
+        <MessageCircleMore className="text-cyan-600/40"/>
+        <p className="hidden md:inline-block text-white">Comments</p>
       </NavLink>
     </div>
   );
