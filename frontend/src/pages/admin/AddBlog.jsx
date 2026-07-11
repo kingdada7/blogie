07c6f39083d33d809a20f8b7b2619f7b5f4fde77
kingdada7 <toluwadada7@gmail.com>
2026-07-11 18:45:36 +0100

    added apperance none styling to select

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -83,7 +83,7 @@ const AddBlog = () => {
         <select
           onChange={(e) => setCategory(e.target.value)}
           name="category"
-          className="mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded"
+          className="mt-2 px-4 py-2 border text-gray-500 border-gray-300 outline-none rounded appearance-none cursor-pointer"
         >
           <option value="">Select category</option>
           {blogCategories.map((item, index) => {
@@ -103,6 +103,7 @@ const AddBlog = () => {
             onChange={e => setIsPublished(e.target.checked)}
           />
         </div>
+        <button type="submit" className="mt-8 w-40 h-10 bg-cyan-600 text-white rounded-md cursor-pointer text-sm">Add Blog</button>
       </div>
     </form>
   );
