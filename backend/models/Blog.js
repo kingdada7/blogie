08dc1fd795385155e90bc07fce089c4625e468ec
kingdada7 <toluwadada7@gmail.com>
2026-07-14 04:31:54 +0100

    tested addblog route on post man - worked

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -1,34 +1,37 @@
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    subTitle:{
-        type:String,
-        required:true
-    },
-
-    description:{
-        type:String,
-        required:true
-    },
-    category:{
-        type:String,
-        required:true
-    },
-    image:{
-        type:String,
-        required:true,
+    subTitle: {
+      type: String,
+      required: true,
     },
 
-    isPublished:{
-        type:String,
-        reqiured:true
-    }
-},{timestamps:true});
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
 
-const Blog = mongoose.model("blog",blogSchema);
+    isPublished: {
+      type: String,
+      reqiured: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Blog = mongoose.model("blog", blogSchema);
 
 export default Blog;
