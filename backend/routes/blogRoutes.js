0fdd5f896fe2617c5d1f35f5623b50939acd7883
kingdada7 <toluwadada7@gmail.com>
2026-07-14 02:19:05 +0100

    added a auth middleware

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { addBlog } from "../controllers/blogControllers";
 import upload from "../middleware/multer.js";
+import auth from "../middleware/auth.js";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", upload.single("image"), addBlog);
+blogRouter.post("/add", upload.single("image"), auth, addBlog);
