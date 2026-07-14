266a6677f79a8bb7e20d260f407d0065155b44dc
kingdada7 <toluwadada7@gmail.com>
2026-07-14 11:26:27 +0100

    added a toggle publish fuction

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -92,3 +92,15 @@ export const deleteBlogById = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+export const togglePublish = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const blog = await Blog.findById(id);
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+    res.json({ success: true, message: "Blog status updated" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
