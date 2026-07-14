347deee72b9d1f973b473aff60feb1418b8bb02d
kingdada7 <toluwadada7@gmail.com>
2026-07-14 02:06:35 +0100

    added multer middleware to blog router

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { addBlog } from "../controllers/blogControllers";
+import upload from "../middleware/multer";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", addBlog);
+blogRouter.post("/add", upload, addBlog);
