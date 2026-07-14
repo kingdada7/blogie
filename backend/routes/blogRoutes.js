3cd702263cc9ef2f32d7fa0b4399a4875ec6fc03
kingdada7 <toluwadada7@gmail.com>
2026-07-14 14:40:07 +0100

    set addcomments and getblogcommets to routes path

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import {
   addBlog,
+  addComment,
   deleteBlogById,
   getAllBlogs,
   getBlogById,
+  getBlogComments,
   togglePublish,
 } from "../controllers/blogControllers.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
-
 
 const blogRouter = express.Router();
 
@@ -17,5 +18,8 @@ blogRouter.get("/all", getAllBlogs);
 blogRouter.get("/:blogId", getBlogById);
 blogRouter.delete("/delete", auth, deleteBlogById);
 blogRouter.post("/toggle-publish", auth, togglePublish);
+
+blogRouter.post("/add-comment", addComment);
+blogRouter.post("/comments", getBlogComments);
 
 export default blogRouter;
