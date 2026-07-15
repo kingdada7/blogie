d31cd282967ebc040c924a87d866644d1ffe66cb
kingdada7 <toluwadada7@gmail.com>
2026-07-15 02:10:12 +0100

    add use context - app context

--- frontend/context/AppContext.jsx
+++ frontend/context/AppContext.jsx
@@ -1,9 +1,13 @@
 import { Children } from "react";
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 const AppContext = createContext();
 
 export default AppProvider = ({ Children }) => {
   const value = {};
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+    return useContext(AppContext)
 };
