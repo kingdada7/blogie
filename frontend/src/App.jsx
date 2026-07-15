12487abbfda09726ce7d2c33c02d15fe4ccdd22a
kingdada7 <toluwadada7@gmail.com>
2026-07-15 02:57:16 +0100

    added toaster in apppage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,12 @@ import ListBlog from "./pages/admin/ListBlog";
 import Comments from "./pages/admin/Comments";
 import Login from "./pages/admin/Login";
 import "quill/dist/quill.snow.css";
+import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
     <div>
       <SpaceScene />
+      <Toaster/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
