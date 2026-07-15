ffa6f2d0fde29d46e4e6594f0d5c637d5e0a54d3
kingdada7 <toluwadada7@gmail.com>
2026-07-15 01:39:24 +0100

    set get all comments to a route

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { adminLogin } from "../controllers/adminControllers.js";
+import { adminLogin, getAllComments } from "../controllers/adminControllers.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login", adminLogin);
+adminRouter.get("/comments", auth, getAllComments);
 
 export default adminRouter;
