7c17c5f999064fe7f708f7c7bf09d8e5f38b60f1
kingdada7 <toluwadada7@gmail.com>
2026-07-15 15:13:00 +0100

    added a clear serch button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,6 +10,11 @@ const Header = () => {
     e.preventDefault();
     setInput(inputRef.current.value);
   };
+
+  const onClear = () => {
+    setInput("");
+    inputRef.current.value = "";
+  };
   return (
     <div className="mx-8 sm:mx-16 xl:mx-24 relative">
       <div className="text-center mt-20 mb-8">
@@ -45,6 +50,16 @@ const Header = () => {
             Search
           </button>
         </form>
+      </div>
+      <div className="text-center">
+        {input && (
+          <button
+            onClick={onClear}
+            className="border text-white font-light text-xs py-1 px-3 rounded-sm shadow-custom-sm cursor-pointer"
+          >
+            Clear Search
+          </button>
+        )}
       </div>
     </div>
   );
