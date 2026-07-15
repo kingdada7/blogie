4abd76ded2a251df3e5a9b377d257cd12f08206f
kingdada7 <toluwadada7@gmail.com>
2026-07-15 01:36:06 +0100

    updated deleteblogby id -blog controller

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -88,6 +88,8 @@ export const deleteBlogById = async (req, res) => {
   try {
     const { id } = req.body;
     await Blog.findByIdAndDelete(id);
+    // delete all comments associated with the blog
+    await Comment.deleteMany({ blog: id });
     res.json({ success: true, message: "Blog deleted sucessfully" });
   } catch (error) {
     res.json({ success: false, message: error.message });
