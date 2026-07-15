f2ccf9e7fc35e25919d6cfb1f48e8f7ba53c8c58
kingdada7 <toluwadada7@gmail.com>
2026-07-15 01:41:27 +0100

    set getall blogs admin to a route

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { adminLogin, getAllComments } from "../controllers/adminControllers.js";
+import {
+  adminLogin,
+  getAllBlogsAdmin,
+  getAllComments,
+} from "../controllers/adminControllers.js";
+import auth from "../middleware/auth.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login", adminLogin);
 adminRouter.get("/comments", auth, getAllComments);
+adminRouter.get("/blogs", auth, getAllBlogsAdmin);
+adminRouter.post("/delete-comment",)
 
 export default adminRouter;
