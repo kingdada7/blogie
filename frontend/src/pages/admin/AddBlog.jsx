b1e627e9aacb54a613afbd6fa6836a6333d3e037
kingdada7 <toluwadada7@gmail.com>
2026-07-16 09:48:49 +0100

    added a setstate to disable button after user request

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 import { assets, blogCategories } from "../../Assets/assets";
 import { Upload } from "lucide-react";
 import Quill from "quill";
+import { useAppContext } from "../../context/AppContext";
 
 const AddBlog = () => {
+  const { axios } = useAppContext();
+  const [isAdding, setIsAdding] = useState(false);
   const editorRef = useRef(null);
   const quillRef = useRef(null);
   const [image, setImage] = useState(false);
@@ -100,10 +103,16 @@ const AddBlog = () => {
             type="checkbox"
             checked={isPublished}
             className="scale-125 cursor-pointer"
-            onChange={e => setIsPublished(e.target.checked)}
+            onChange={(e) => setIsPublished(e.target.checked)}
           />
         </div>
-        <button type="submit" className="mt-8 w-40 h-10 bg-cyan-600 text-white rounded-md cursor-pointer text-sm">Add Blog</button>
+        <button
+          type="submit"
+          disabled={isAdding}
+          className="mt-8 w-40 h-10 bg-cyan-600 text-white rounded-md cursor-pointer text-sm"
+        >
+          {isAdding ? "Adding..." : "Add Blog"}
+        </button>
       </div>
     </form>
   );
