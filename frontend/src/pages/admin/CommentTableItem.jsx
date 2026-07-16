eb3f598250644a7631618c3a13d96240bd44c985
kingdada7 <toluwadada7@gmail.com>
2026-07-16 15:32:30 +0100

    delete comment fuction added to comment table

--- frontend/src/pages/admin/CommentTableItem.jsx
+++ frontend/src/pages/admin/CommentTableItem.jsx
@@ -24,6 +24,26 @@ const CommentTableItem = ({ comment, fetchComments }) => {
     }
   };
 
+  const deleteComment = async () => {
+    try {
+      const confirm = window.confirm(
+        "Are you sure want to delete this comment",
+      );
+      if (!confirm) return;
+      const { data } = await axios.post("api/admin/delete-comment", {
+        id: _id,
+      });
+      if (data.success) {
+        toast.success(data.message);
+        fetchComments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <tr className="order-y" border-gray-300>
       <td className="px-6 py-4">
