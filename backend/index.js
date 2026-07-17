8d1fea324f9b02abfc9503213ec6693a2a193b6a
kingdada7 <toluwadada7@gmail.com>
2026-07-17 02:32:02 +0100

    deployment

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL ,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
